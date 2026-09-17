76d374e222947def73d338e6e59888cb297986e3
HoangTranQuanTrong <96651439+quantrong77@users.noreply.github.com>
2026-09-17 09:34:11 +0700

    Set Supabase URL and anon key in config.js
    Updated Supabase configuration with new URL and anon key.

--- config.js
+++ config.js
@@ -19,7 +19,7 @@
 window.DGA_CONFIG = {
   GSHEET_WEBAPP_URL:"https://script.google.com/macros/s/AKfycbzZM6-fkFkWvfX1G4WHMhzT7p5VFB5ZsT3KBHaO-BLJWtKiNoW9qTDy4RonOSyx4hyp/exec",
   GSHEET_SHEET_URL: "https://docs.google.com/spreadsheets/d/1Pm5Nq5wY-1sBDUl5DFNBtxfGqH_Ij_SsZIAJQJ0WaAU/edit", // Dán link Google Sheet (docs.google.com/spreadsheets/d/...) để bấm badge "Database" mở nhanh Sheet
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://sfnnbdyazqguwnubzjhg.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmbm5iZHlhenFndXdudWJ6amhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2MDQ2MDEsImV4cCI6MjEwNTE4MDYwMX0.8mAzu7EGp8vsiglPFIL7Y70yCUg-qt7g2Gt_PbXF0xk",
   GOOGLE_CLIENT_ID: "162684736346-spsmoiqgk6sp3k5d8l24h85p1mcd5cja.apps.googleusercontent.com",
 };
